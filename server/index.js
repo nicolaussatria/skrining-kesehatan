f6974a4bfaca7f15e4ab5739935624488a431bae
nicolaussatria <nicolaus.satria1@gmail.com>
2024-09-16 18:02:24 +0700

    zz

--- server/index.js
+++ server/index.js
@@ -1,223 +1,37 @@
-// backend/server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { evaluateRisk } = require('./models/riskCriteria');
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
-// Replace with your actual MongoDB connection string
-const MONGO_ATLAS_URL = process.env.MONGO_ATLAS_URL || 'mongodb+srv://nicolaussatria:gerobakijo333@cluster.gnwjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster';
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Connect to MongoDB
-mongoose.connect(MONGO_ATLAS_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
+var app = express();
 
-// Mock data stores
-let users = [];
-let questions = [
-  {
-    questionText: 'Berapakah hasil tekanan darah ibu terakhir yang diukur oleh petugas RS atau Puskesmas?',
-    options: [],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'input',
-    unit: 'mmHg',
-  },
-  {
-    questionText: 'Apakah anda mengalami kondisi sulit tidur/cemas belebih?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda mengalami stress emosional, dan kondisi tertekan belakangan ini?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda merasa pusing atau sering mengalami sakit kepala hebat yang tidak biasa?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda mengalami keluar cairan dari jalan lahir?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda merasa sesak napas atau sulit bernapas?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda mengalami kontraksi rahim atau nyeri yang berulang-ulang?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda mengalami perubahan mendadak pada penglihatan, seperti kilatan cahaya atau penglihatan kabur?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Anda mengalami pembengkakan pada area telapak kaki atau wajah?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pertanyaan Klinis Kondisi Pasien',
-    type: 'radio',
-  },
-  // Riwayat Kesehatan Diri
-  {
-    questionText: 'Apakah Anda mengalami tekanan darah tinggi sebelumnya atau memiliki riwayat preeklampsia?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sedang/pernah mengidap penyakit Diabetes Melitus (kencing manis)?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sedang/pernah mengidap penyakit ginjal?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sedang/pernah mengidap penyakit auto imun atau sakit lupus?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah mempunyai kebiasaan merokok sebelum hamil?',
-    options: ['Tidak', 'Dulu saya pernah merokok tetapi saat ini sudah berhenti'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah suami atau keluarga satu rumah anda aktif dalam merokok?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah ini adalah kehamilan pertama anda?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  {
-    questionText: 'Jika anda sudah pernah melahirkan sebelumnya, berapakah jarak kehamilan terakhir dengan kehamilan saat ini?',
-    options: ['jarak kehamilan > 2 tahun - 10 tahun', 'Jarak kehamilan > 10 tahun', 'jarak kehamilan < 2 tahun'],
-    category: 'Riwayat Kesehatan Diri',
-    type: 'radio',
-  },
-  // Riwayat Kesehatan Keluarga
-  {
-    questionText: 'Apakah Ibu atau saudara perempuan anda mempunyai penyakit hipertensi/darah tinggi?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Keluarga',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah Ibu atau saudara perempuan anda mempunyai penyakit diabetes mellitus/ kencing manis?',
-    options: ['Ya', 'Tidak'],
-    category: 'Riwayat Kesehatan Keluarga',
-    type: 'radio',
-  },
-  // Pola Konsumsi Makanan
-  {
-    questionText: 'Apakah anda mempunyai kebiasaan makan makanan yang berasa asin?',
-    options: ['Ya', 'Kadang - kadang saya mengkonsumsi makanan yang berasa asin (seminggu 3 kali)', 'Ya, hampir setiap hari saya mengkonsumsi makanan yang berasa asin'],
-    category: 'Pola Konsumsi Makanan',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda mempunyai kebiasaan mengkonsumsi kopi sehari hari?',
-    options: ['Tidak, saya tidak pernah mengkonsumsi kopi', 'Sebulan 1-2 kali saya mengkonsumsi kopi', 'Ya, hampir setiap hari saya mengkonsumsi kopi'],
-    category: 'Pola Konsumsi Makanan',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sering mengkonsumsi makanan berlemak / bersantan sehari hari?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pola Konsumsi Makanan',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sering mengkonsumsi makanan cepat saji (KFC, McDonald, dll) sehari hari?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pola Konsumsi Makanan',
-    type: 'radio',
-  },
-  {
-    questionText: 'Apakah anda sering mengkonsumsi minuman manis / minuman kemasan sehari hari?',
-    options: ['Ya', 'Tidak'],
-    category: 'Pola Konsumsi Makanan',
-    type: 'radio',
-  },
+const whitelist = [
+  '*'
 ];
 
-// Routes
-app.post('/api/users', (req, res) => {
-  console.log('Received data:', req.body);
-  const { bpjsNumber, weight, height, education, familyContact, healthQuestions } = req.body;
-  const riskLevel = evaluateRisk(healthQuestions);
-
-  const newUser = {
-    id: users.length + 1,
-    bpjsNumber,
-    weight,
-    height,
-    education,
-    familyContact,
-    healthQuestions,
-    riskLevel,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  users.push(newUser);
-  res.json({ success: true, userId: newUser.id });
-});
-
-app.get('/api/users', (req, res) => {
-  res.json(users);
-});
-
-app.get('/api/users/:id', (req, res) => {
-  const user = users.find((u) => u.id == req.params.id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ success: false, message: 'User not found' });
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
   }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
 });
 
-app.get('/api/questions', (req, res) => {
-  res.json(questions);
-});
-
-// Start the server
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
 
 
 
+module.exports = app;

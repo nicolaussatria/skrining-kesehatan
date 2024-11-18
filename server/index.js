bb1d36e58810291ac7c7327563b89db118457a1e
nicolaussatria <nicolaus.satria1@gmail.com>
2024-11-18 10:57:49 +0700

    test supaszs2ss2s

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { QuestionsService, UserService } = require('./services/supabaseClient');
+const { QuestionsService, UserService } = require('./supabaseClient');
 
 const app = express();
 
